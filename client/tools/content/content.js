de46be368e858e50339fa29cf649d85082280d99
Breck <breck7@gmail.com>
2013-09-05 14:19:02 -0700

    Content Editor

--- client/tools/content/content.js
+++ client/tools/content/content.js
@@ -1,5 +1,31 @@
 var Content = new Tool('Content')
 Content.set('description', 'Edit the content on your pages.')
 Content.set('icon', 'font')
+Content.set('pages')
 
 
+Content.downloadPages = function () {
+  Explorer.folderToSpace('private/pages', function (data) {
+    var space = new Space(data)
+    space.keys = space.keys.sort(function (a, b) {
+      return b > a
+    })
+    space.each(function (filename, value) {
+      Content.set('pages ' + filename, new Page(value))
+    })
+    Content.trigger('pages')
+  })
+}
+
+Content.listPages = function () {
+  var pages = Content.get('pages')
+  $('#ContentPages').html('')
+  
+  
+  pages.each(function (filename, value) {
+    $('#ContentPages').append('<li><a class="cursor" onclick="Content.Editor.open(\'' + filename + '\')">' + filename + '</a></li>')
+  })
+}
+
+Content.on('ready', Content.downloadPages)
+Content.on('pages', Content.listPages)
---
+++ client/tools/content/editor.js
@@ -0,0 +1,54 @@
+(function () {
+var Editor = {}
+/** Start of file */
+
+var filter = function () {
+  if (this.div.tag === 'script')
+    return ''
+  // Remove JS
+  _.each(this.div.attrs, function (value, key, obj) {
+    if (key.match(/^on/))
+      delete obj[key]
+  })
+  return this.div.toHtml()
+}
+
+Editor.open = function (filename) {
+  var page = Content.get('pages ' + filename)
+  
+  var html = ''
+  page.each(function (id, scrap) {
+    html += '\n  ' + scrap.toHtml(filter)
+  })
+
+  $('#ContentStage').contents().find('body').html(html)
+  
+  $('#ContentStage').contents().find('body').on('click', 'a', function (event) {
+    event.preventDefault()
+    return false
+  })
+  
+  $('#ContentStage').contents().find('body').on('click', '*', function (event) {
+    var text = $(this).text()
+    if (!text.length)
+      return true
+    $(this).attr('contenteditable', true)
+    $(this).focus()
+    $(this).on('blur', function () {
+      
+    })
+  })
+  
+  //  var host = document.querySelector('#ContentStage')
+  //  var root = host.webkitCreateShadowRoot()
+  //  root.innerHTML = page.toHtml()  
+//  $('#ContentStage')[0].innerHTML = html
+}
+
+
+
+
+
+
+/** End of file */
+Content.Editor = Editor})()
