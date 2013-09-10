ff868f28b5ce3c87f773129d5ea6a77f1a1d15b1
Breck <breck7@gmail.com>
2013-09-10 15:06:07 -0700

    Removed IRC tool

--- client/tools/irc/tool.js
+++
@@ -1,9 +0,0 @@
-var Irc = new Tool('Irc')
-Irc.set('description', 'Chat with other NudgePad users.')
-Irc.set('beta', true)
-Irc.set('icon', 'comment')
-
-Irc.on('ready', function () {
-  if (!$('#IrcChat').attr('src'))
-    $('#IrcChat').attr('src', 'http://webchat.freenode.net?randomnick=1&channels=%23%23nudgepad')
-})
