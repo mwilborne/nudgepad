c6617b1041ae7fa5ac5b2b3a77b6aafbad025ac6
Breck <breck7@gmail.com>
2013-10-04 17:43:37 -0700

    Create password for ssh access #396

--- system/createUser.sh
+++ system/createUser.sh
@@ -7,4 +7,5 @@ PW=`echo $RANDOM$RANDOM`
 setUsername $domain
 useradd -m -p "$PW" -G projects $username
 usermod -a -G $username $serverUser
+echo $PW > $projectsPath/$domain/nudgepad/pw.txt
 chown -R $username:$username $projectsPath/$domain
