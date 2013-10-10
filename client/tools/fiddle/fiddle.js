f1ca969e78f8f4c47a3f6f46ec84dcd5ee2b3d3c
Breck <breck7@gmail.com>
2013-10-09 22:37:35 -0700

    Added another row to fiddle

--- client/tools/fiddle/fiddle.js
+++ client/tools/fiddle/fiddle.js
@@ -1,15 +1,36 @@
 var Fiddle = new Tool('Fiddle')
 
-Fiddle.A = function () {console.log(3)}
+Fiddle.A = function () {}
 Fiddle.B = function () {}
 Fiddle.C = function () {}
+Fiddle.D = function () {}
+Fiddle.E = function () {}
+Fiddle.F = function () {}
 
 Fiddle.keyup = function () {
   var name = $(this).attr('name')
   Fiddle[name].call(this)
+  Fiddle.save()
+}
+
+Fiddle.iframe = function () {
+  return $('#F').contents().find('body')
 }
 
 Fiddle.on('ready', function () {
+  
+  if (store.get('Fiddle')) {
+    var space = new Space(store.get('Fiddle'))
+    space.get('values').each(function (key, value) {
+      $('#' + key).val(value)
+    })
+    if (space.get('code')) {
+      space.get('code').each(function (letter, value) {
+        $('.FiddleCode[name=' + letter+ ']').val(value)
+      })
+    }
+    // load
+  }
   
   $('.FiddleTextarea').on('keyup', Fiddle.keyup)
   
@@ -18,5 +39,23 @@ Fiddle.on('ready', function () {
     var code = $(this).val()
     var js = 'Fiddle.' + cell + ' = function () { ' + code + '}'
     eval(js)
+    Fiddle.save()
   })
+  
+  $('.FiddleCode').trigger('change')
 })
+
+Fiddle.save = function () {
+  var space = new Space()
+  var array = 'A B C D E F'.split(/ /g)
+  array.forEach(function (letter) {
+    var val = $('#' + letter).val()
+    if (val)
+      space.set('values ' + letter, val)
+    var code = $('.FiddleCode[name=' + letter+ ']').val()
+    if (code)
+      space.set('code ' + letter, code)
+  })
+  store.set('Fiddle', space.toString())
+}
+
