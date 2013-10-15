8c42353a775817d2a3ae113d34ee4612fb3378e9
Breck <breck7@gmail.com>
2013-10-15 10:32:50 -0700

    Scraps update

--- client/public/js/scraps.js
+++ client/public/js/scraps.js
@@ -3,11 +3,11 @@ if (typeof exports !== 'undefined')
   var Space = require('space')
 
 var Scraps = {}
-Scraps.version = '0.8.2'
+Scraps.version = '0.8.3'
 
 // Words that appear in both: title style
 Scraps.tags = {}
-Scraps.tagsArray = 'a abbr address area article aside audio b base bdi bdo blockquote body br button canvas caption cite code col colgroup command datalist dd del details dfn div dl dt em embed fieldset figcaption figure footer form h1 h2 h3 h4 h5 h6 head header hgroup hr html i iframe img input ins kbd keygen label legend li link map mark menu meta meter nav noscript object ol optgroup option output p param pre progress q rp rt ruby s samp script section select small source span strong sub summary sup table tbody td tfoot th thead tr track u ul var video wbr'.split(/ /g)
+Scraps.tagsArray = 'a abbr address area article aside audio b base bdi bdo blockquote body br button canvas caption cite code col colgroup command datalist dd del details dfn div dl dt em embed fieldset figcaption figure footer form h1 h2 h3 h4 h5 h6 head header hgroup hr html i iframe img input ins kbd keygen label legend li link map mark menu meta meter nav noscript object ol optgroup option output p param pre progress q rp rt ruby s samp script section select small source span strong sub summary sup table tbody td textarea tfoot th thead tr track u ul var video wbr'.split(/ /g)
 Scraps.tagsArray.forEach(function (tag, i) {
   Scraps.tags[tag] = true
 })
